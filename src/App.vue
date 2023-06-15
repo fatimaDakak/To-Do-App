4c85e320507dc770cf9240e8f0f00c12b1c1a7f5
FATIMA DAKAK <fatima.dakak@etu.uae.ac.ma>
2023-06-15 19:21:07 +0100

    added crud operations

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,109 @@
 <template>
-  <main class="app">
-    <section class="greeting">
-      <h2 class="title">
-        What's up
-        <input type="text" placeholder="name here" v-model="name" />
-      </h2>
-    </section>
-    <section class="createToDo">
-      <h3>Create a To Do</h3>
-      <form @submit.prevent="addTodo">
-        <h4>What's on your to-do list</h4>
-        <input
-         type="text" 
-         placeholder="e.g make a video" 
-         v-model="input_content" />
-        <h4>Pick a categorie</h4>
-        <div class="options">
-        <input type="radio" name="category" value="Business" v-mode="input_category">
-        </div>
-      </form>
-    </section>
-  </main>
+  <div class="container">
+    <h2 class="text-center mt-5">My vue ToDo App</h2>
+    <!--input-->
+    <div class="d-flex">
+      <input type="text" class="form-control m-2" placeholder="add your todo here" v-model="task">
+      <button @click="AddTask" class="btn btn-secondary col-lg-2  m-2">Submit </button>
+           
+
+    </div>
+    <!--todo-->
+    <table class="table table-bordered mt-5 container">
+  <thead>
+    <tr>
+      <th scope="col">Task</th>
+      <th scope="col">Status</th>
+      <th scope="col">Update</th>
+      <th scope="col">Delete</th>
+    </tr>
+  </thead>
+  <tbody v-for="(task, index) in tasks" :key="task">
+
+    <tr>
+      <th scope="row"><span :class="{'finished' : task.status=== 'finished'}">{{task.name}}</span></th>
+      <td>
+        <span  :class="{'text-danger' : task.status=== 'to-do','text-warning': task.status=== 'in-progress' }"
+       class="pointer" @click="changeStatus(index)">
+       {{firstCharUpper(task.status)}}
+       </span>
+       </td>
+      <td><button @click=" setEditTask(index)"><i class="fas fa-pen-square"></i></button></td>
+      <td><button @click="deleteTask(index)"><i class="far fa-trash-alt"></i></button></td>
+    </tr>
+    
+   
+  </tbody>
+</table>
+
+  </div>
 </template>
 
 <script>
-import { onMounted, ref, computed, watch } from 'vue';
-
 export default {
-  name: 'App',
-  setup() {
-    const name = ref('');
-    const todos = ref([]);
-    const input_content = ref('');
-   const input_category = ref(null);
-    const addTodo = () => {
-      // Add logic for adding a todo
-    };
-
-    const sortedTodos = computed(() => todos.value.sort((a, b) => b.createAt - a.createAt));
-
-    watch(name, (newVal) => {
-      localStorage.setItem('name', newVal);
-    });
-
-    onMounted(() => {
-      name.value = localStorage.getItem('name');
-    });
-
+  name: "App",
+  data() {
     return {
-      name,
-      todos,
-      input_content,
-      input_category,
-      addTodo,
-      todos_asc: sortedTodos,
-    };
+      task: '',
+      tasks: [
+        {
+          name: "Practice Vue.js",
+          status: 'to-do'
+        },
+        {
+          name: "Have lunch with my mother",
+          status: 'to-do'
+        }
+      ],
+      available_stat: [
+        'to-do',
+        'in-progress',
+        'finished'
+      ],
+      editTaskIndex: null,
+    }
   },
-};
+  methods: {
+    AddTask() {
+      if (this.task.length === 0) return;
+
+      if (this.editTaskIndex === null) {
+        this.tasks.push({
+          name: this.task,
+          status: 'to-do'
+        });
+      } else {
+        this.tasks[this.editTaskIndex].name = this.task;
+        this.editTaskIndex = null;
+      }
+
+      this.task = ''; // Reset the input field after adding/updating the task
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+    setEditTask(index) { // Renamed the method to setEditTask
+      this.task = this.tasks[index].name;
+      this.editTaskIndex = index;
+    },
+    changeStatus(index) {
+      let newIndex = this.available_stat.indexOf(this.tasks[index].status);
+      if (++newIndex > 2) newIndex = 0;
+      this.tasks[index].status = this.available_stat[newIndex];
+    },
+    firstCharUpper(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+  }
+}
 </script>
 
-<style></style>
+
+<style>
+.pointer {
+  cursor: pointer;
+}
+.finished{
+  text-decoration: line-through;
+}
+</style>
